b0805dcfeb8a7eec4d0402b50b70d3cb271878c1
adriijg <adrianjunquera@hotmail.es>
2026-02-24 01:00:24 +0100

    changable_hitboxes

--- samurai/samurai.gd
+++ samurai/samurai.gd
@@ -1,56 +1,55 @@
 extends CharacterBody2D
 
-@export var gravity_scale = 2
 @export var speed = 500
-@export var acceleration = 600
-@export var friction = 1000
+@export var gravity_scale = 2
 @export var jump_force = -700
-@export var air_acceleration = 2000
-@export var air_friction = 700
 
-@onready var ani_samurai = $ani_samurai
-
-func apply_gravity(delta):
-	if not is_on_floor():
-		velocity+=get_gravity() * delta * gravity_scale
-
-func handle_acceleration(input_axis, delta):
-	if not is_on_floor(): return
-	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, speed*input_axis, acceleration*delta)
-
-func apply_friction(input_axis, delta):
-	if input_axis==0 and is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, friction*delta)
-
-func handle_jump():
-	if is_on_floor():
-		if Input.is_action_pressed("saltar"):
-			velocity.y = jump_force
-
-func handle_air_acceleration(input_axis, delta):
-	if is_on_floor(): return
-	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, speed*input_axis, air_acceleration *delta)
-
-func update_animation(input_axis):
-	if input_axis !=0:
-		# velocidad de la animación será dependiente de la velocidad
-		ani_samurai.speed_scale = velocity.length()/100
-		ani_samurai.flip_h = (input_axis<0)
-		ani_samurai.play("run")
-	elif not is_on_floor():
-		ani_samurai.play("jump")
-	else:
-		ani_samurai.speed_scale=1
-		ani_samurai.play("idle")
+@onready var visual = $Visual
+@onready var ani_samurai = $Visual/ani_samurai
+@onready var col_normal = $col_samurai_normal # Ruta corregida a hija directa
+@onready var col_run = $Visual/col_samurai_run
+@onready var col_attack = $Visual/col_samurai_attack
 
 func _physics_process(delta: float) -> void:
-	var input_axis = Input.get_axis("mover_izquierda","mover_derecha")
-	apply_gravity(delta)
-	handle_acceleration(input_axis, delta)
-	apply_friction(input_axis, delta)
-	handle_jump()
-	handle_air_acceleration(input_axis, delta)
-	update_animation(input_axis)
+	var input_axis = Input.get_axis("mover_izquierda", "mover_derecha")
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta * gravity_scale
+	
+	if is_on_floor() and Input.is_action_just_pressed("saltar"):
+		velocity.y = jump_force
+
+	velocity.x = input_axis * speed
+	
 	move_and_slide()
+	update_animation(input_axis)
+	update_col()
+
+func update_animation(input_axis):
+	if Input.is_action_just_pressed("atacar"):
+		ani_samurai.play("atacar")
+		return
+
+	if ani_samurai.animation == "atacar" and ani_samurai.is_playing():
+		return
+
+	if input_axis != 0:
+		# Giramos solo el contenedor visual y las hitboxes de ataque
+		visual.scale.x = 1 if input_axis > 0 else -1
+		ani_samurai.play("run")
+	elif is_on_floor():
+		ani_samurai.play("idle")
+
+func update_col():
+	var anim = ani_samurai.animation
+	
+	# La normal siempre activa para pisar el suelo con seguridad
+	col_normal.disabled = false
+	col_run.disabled = true
+	col_attack.disabled = true
+	
+	if anim == "run":
+		# Solo activamos la de correr si es realmente distinta
+		col_run.disabled = false
+	elif anim == "atacar":
+		col_attack.disabled = false
