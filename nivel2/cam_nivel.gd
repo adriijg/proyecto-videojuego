5d43e859e6506633bd27254ee5e13cc9511c1ae3
DavidHoyas <thetxaby@gmail.com>
2026-02-24 04:26:10 +0100

    Revert "Merge branch 'features_david' into develop"
    This reverts commit 7cfa16fd39da930e77c81bec1cdf50d722974c4e, reversing
    changes made to eee9e6a8077e367d97d626b477a9b2515f5b6a9f.

--- nivel2/cam_nivel.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-func _ready():
-	enabled = true           # ACTIVA
-	make_current()           # FUERZA ser la única cámara
-	reset_smoothing()        # Reinicia smoothing
-	print("🎥 CameraNivelMedio ACTIVADA - Límites: ", limit_right - limit_left)
