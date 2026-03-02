4e3ab9eafe4fc0d869a5d26afd5853136701ac6a
DavidHoyas <thetxaby@gmail.com>
2026-03-02 23:04:57 +0100

    retoques finales v3

--- Global.gd
+++ Global.gd
@@ -10,7 +10,8 @@ var niveles = [
 	"res://niveles/nivel1/environment.tscn",
 	"res://niveles/nivel2/nivel_medio.tscn",
 	"res://niveles/nivel3/nivel_3.tscn",
-	"res://niveles/mapa_final/envi_final.tscn"
+	"res://niveles/mapa_final/envi_final.tscn",
+	"res://niveles/mapa_victoria/victoria.tscn"
 ]
 
 func ir_al_siguiente_nivel():
